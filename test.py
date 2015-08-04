2739b3871c1f4b99e1874a287371873e85cb06d0
rsgoodwin <randall_s_goodwin@yahoo.com>
2015-08-03 19:09:09 -0700

    minor format changes

--- test.py
+++ test.py
@@ -26,11 +26,10 @@ p2 = make_stripplot(x, y, dt, .3, True)
 p3 = make_boxplot(x, y, st, dt, .3, .7, True, True, True)
 p4 = make_boxplot(x, y, st, dt, .3, .5, False, False, True)
 
-p5 = make_violinplot(x=x, y=y, st=st, dt=dt, jw=.2, vw=.8, bw_method=.2,
-                     jitter_points=True, show_points=True, show_outliers=True)
+p5 = make_violinplot(x=x, y=y, st=st, dt=dt, jw=.2, vw=.8, bw_method=.2, jitter_points=True, show_points=True,
+                     show_outliers=True)
 
-p6 = make_violinplot(x=x, y=y, st=st, dt=dt, jw=.2, vw=.7,
-                     jitter_points=False, show_points=False, show_outliers=True)
+p6 = make_violinplot(x=x, y=y, st=st, dt=dt, jw=.2, vw=.7, jitter_points=False, show_points=False, show_outliers=True)
 
 # get some seaborn data for comparison
 tips = sns.load_dataset("tips")
@@ -38,18 +37,14 @@ x, y = 'day', 'total_bill'
 
 st, dt = get_summary_stats('day', 'total_bill', tips)
 
-p7 = make_boxplot(x=x, y=y, st=st, dt=dt, jw=.15, bw=.7,
-                  jitter_points=True, show_points=True, show_outliers=True)
+p7 = make_boxplot(x=x, y=y, st=st, dt=dt, jw=.15, bw=.7, jitter_points=True, show_points=True, show_outliers=True)
 
-p8 = make_boxplot(x=x, y=y, st=st, dt=dt, jw=.15, bw=.7,
-                  jitter_points=False, show_points=False, show_outliers=True)
+p8 = make_boxplot(x=x, y=y, st=st, dt=dt, jw=.15, bw=.7, jitter_points=False, show_points=False, show_outliers=True)
 
-p9 = make_violinplot(x=x, y=y, st=st, dt=dt, jw=.2, vw=.8,
-                     jitter_points=False, show_points=True, show_outliers=True,
+p9 = make_violinplot(x=x, y=y, st=st, dt=dt, jw=.2, vw=.8, jitter_points=False, show_points=True, show_outliers=True,
                      bw_method=.3)
 
-p10 = make_violinplot(x=x, y=y, st=st, dt=dt, jw=0, vw=.6,
-                      jitter_points=False, show_points=False, show_outliers=True)
+p10 = make_violinplot(x=x, y=y, st=st, dt=dt, jw=0, vw=.6, jitter_points=False, show_points=False, show_outliers=True)
 
 # render the plots
 p = gridplot([[p1, p2], [p3, p4], [p5, p6], [p7, p8], [p9, p10]])
