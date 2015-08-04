b56cc17a305fb1bc346bacb90a1ce8f03bb21d5b
rsgoodwin <randall_s_goodwin@yahoo.com>
2015-08-03 19:08:15 -0700

    changed 25%, 50%, 75% to q1, med, q3 for clarity

--- summarize_df.py
+++ summarize_df.py
@@ -10,16 +10,20 @@ def get_summary_stats(x=None, y=None, data=None):
     # make a group by object to be used later.
     dtg = dt.groupby(x)[y]
 
-    st = dtg.agg([('25%', lambda x: np.nanpercentile(x, 25)),
-                  ('50%', lambda x: np.nanpercentile(x, 50)),
-                  ('75%', lambda x: np.nanpercentile(x, 75))])
+    st = dtg.agg([('q1', lambda x: np.percentile(x, 25)),
+                  ('med', lambda x: np.percentile(x, 50)),
+                  ('q3', lambda x: np.percentile(x, 75))])
 
-    # compute the iqr and upper, lower whisker limits for each level of the group
-    st['iqr'] = st['75%'] - st['25%']
-    st['lwl'] = st['25%'] - 1.5 * st['iqr']
-    st['uwl'] = st['75%'] + 1.5 * st['iqr']
+    """
+    compute the iqr and upper, lower whisker limits for each level of the group
+    note:  the whiskers are not drawn to these points.  Whiskers extend up to the
+    highest non outlier datum and down to the lowest non outlier dataum.
+    """
 
-    # TODO:  Make separate function to compute and flag outliers.
+    st['iqr'] = st['q3'] - st['q1']
+    st['lwl'] = st['q1'] - 1.5 * st['iqr']
+    st['uwl'] = st['q3'] + 1.5 * st['iqr']
+
     # join the uwl and lwl to the raw data, check if outlier and store boolean with data table
     dt = dt.join(st[['lwl', 'uwl']], on=x)
     dt['outlier'] = ~dt[y].between(dt['lwl'], dt['uwl'])
