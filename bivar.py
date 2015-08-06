d523203a714ffdac15bc7d4ab1a42c4174a2c3db
rsgoodwin <randall_s_goodwin@yahoo.com>
2015-08-06 16:34:56 -0700

    added more default styles.  overdue for converting to a class

--- bivar.py
+++ bivar.py
@@ -3,10 +3,8 @@ from scipy import stats
 from bokeh.plotting import figure
 from summarize_df import get_summary_stats
 
-# TODO:
 
-
-def make_base_plot(x=None, y=None, data=None, tools='', width=450, height=300):
+def make_base_plot(x=None, y=None, data=None, width=640, height=480, tools=''):
 
     # create a figure (allow setting)
     p = figure(x_range=list(data[x].cat.categories), tools=tools, width=width, height=height)
@@ -16,7 +14,7 @@ def make_base_plot(x=None, y=None, data=None, tools='', width=450, height=300):
     p.outline_line_width = 1
 
     # styling - title
-    p.title = y + ' vs.' + x
+    p.title = y + ' vs. ' + x
     p.title_text_font_size = "12pt"
 
     # styling - grid lines
@@ -40,22 +38,26 @@ def make_base_plot(x=None, y=None, data=None, tools='', width=450, height=300):
     return p
 
 
-def make_stripplot(x=None, y=None, data=None, jitter_points=False, jw=0.3):
+def make_strip_plot(x=None, y=None, data=None, jitter_points=False, jw=0.3):
+
+    # subset to the columns of interest and drop missing
+    dt = data[[x, y]].dropna()
 
     # create a figure
-    p = make_base_plot(x=x, y=y, data=data)
+    p = make_base_plot(x=x, y=y, data=dt)
 
     if jitter_points:
-        p.scatter(data[x].cat.codes+1 + (np.random.random(len(data))-0.5) * jw, data[y])
+        p.scatter(dt[x].cat.codes+1 + (np.random.random(len(dt))-0.5) * jw, dt[y])
 
     else:
-        p.scatter(data[x].cat.codes+1, data[y])
+        p.scatter(dt[x].cat.codes+1, dt[y])
 
     return p
 
 
-def make_boxplot(x=None, y=None, data=None, jw=.3, bw=.6,
-                 jitter_points=True, show_points=True, show_outliers=True):
+def make_box_plot(x=None, y=None, data=None, jw=.3, bw=.6, jitter_points=True, show_points=False, show_outliers=True,
+                  box_fill_color='lightgrey', box_line_color='grey', box_line_width=1,
+                  whisker_line_color='grey', whisker_line_width=1):
 
     # make a data frame of just the variables needed,  and drop missing values.
     dt = data[[x, y]].dropna()
@@ -65,6 +67,37 @@ def make_boxplot(x=None, y=None, data=None, jw=.3, bw=.6,
 
     # make the base plot
     p = make_base_plot(x=x, y=y, data=dt)
+
+    # ************************
+    # *** Box and Whiskers ***
+    # ************************
+
+    # get centered locations of the boxes
+    st['xc'] = st.index.codes+1
+
+    # plot boxes (left, right, top, bottom)
+    p.quad(st['xc']-bw/2,  st['xc']+bw/2, st['q3'], st['q1'], fill_color=box_fill_color,
+           line_color=box_line_color, line_width=box_line_width)
+
+    # plot median
+    p.segment(st['xc']-bw/2, st['med'], st['xc']+bw/2, st['med'],
+              line_color=whisker_line_color, line_width=whisker_line_width)
+
+    # plot lower and upper whiskers (x0, y0, x1, y1)
+    p.segment(st['xc'], st['q1'], st['xc'], st['lwp'],
+              line_color=whisker_line_color, line_width=whisker_line_width)
+    p.segment(st['xc'], st['q3'], st['xc'], st['uwp'],
+              line_color=whisker_line_color, line_width=whisker_line_width)
+
+    # plot lower and upper whisker terminations
+    p.segment(st['xc']-bw/6, st['uwp'], st['xc']+bw/6, st['uwp'],
+              line_color=whisker_line_color, line_width=whisker_line_width)
+    p.segment(st['xc']-bw/6, st['lwp'], st['xc']+bw/6, st['lwp'],
+              line_color=whisker_line_color, line_width=whisker_line_width)
+
+    # ******************
+    # *** Raw Points ***
+    # ******************
 
     # jitter categorical data
     if jitter_points:
@@ -82,33 +115,11 @@ def make_boxplot(x=None, y=None, data=None, jw=.3, bw=.6,
     if show_outliers:
         p.scatter(dt[dt.outlier]['xc'], dt[dt.outlier][y], color='red', marker='x', size=4, alpha=1)
 
-    # ************************
-    # *** Box and Whiskers ***
-    # ************************
-
-    # get centered locations of the boxes
-    st['xc'] = st.index.codes+1
-
-    # plot boxes (left, right, top, bottom)
-    p.quad(st['xc']-bw/2,  st['xc']+bw/2, st['q3'], st['q1'], color='black', alpha=0.5, fill_color='grey')
-
-    # plot median
-    p.segment(st['xc']-bw/2, st['med'], st['xc']+bw/2, st['med'], color='black')
-
-    # plot lower and upper whiskers (x0, y0, x1, y1)
-    p.segment(st['xc'], st['q1'], st['xc'], st['lwp'], color='black')
-    p.segment(st['xc'], st['q3'], st['xc'], st['uwp'], color='black')
-
-    # plot lower and upper whisker terminations
-    p.segment(st['xc']-bw/6, st['uwp'], st['xc']+bw/6, st['uwp'], color='black')
-    p.segment(st['xc']-bw/6, st['lwp'], st['xc']+bw/6, st['lwp'], color='black')
-
     return p
 
 
-def make_violinplot(x=None, y=None, data=None, jw=.3, vw=.8,
-                    jitter_points=True, show_points=True, show_outliers=True,
-                    bw_method='scott'):
+def make_violin_plot(x=None, y=None, data=None, jw=.3, vw=.8, jitter_points=True, show_points=False,
+                     show_outliers=True, bw_method='scott', violin_padding=.1, grid_points=50):
 
     # make a data frame of just the variables needed,  and drop missing values.
     dt = data[[x, y]].dropna()
@@ -119,35 +130,38 @@ def make_violinplot(x=None, y=None, data=None, jw=.3, vw=.8,
     # make the base plot
     p = make_base_plot(x=x, y=y, data=dt)
 
-    # draw the violins.  Better way?
+    # ***************
+    # *** Violins ***
+    # ***************
+
     ix = 1
-    for cat in dt[x].cat.categories:
+    for category in dt[x].cat.categories:
 
         # subset values and get kde function.
-        yd = dt[dt[x] == cat][y]
+        y_data = dt[dt[x] == category][y]
 
-        # define the point within the range over which to compute the pdf.
-        ymin, ymax = yd.min(), yd.max()
-        yrange = ymax-ymin
-        padding = yrange * 0.1
-        yp = np.linspace(ymin-padding, ymax+padding, 100)
+        # define the points within the range over which to compute the pdf.
+        y_min, y_max = y_data.min(), y_data.max()
+        y_padding = (y_max - y_min) * violin_padding
+        y_grid = np.linspace(y_min - y_padding, y_max + y_padding, grid_points)
 
-        # get the kde function fot the data
-        pdf = stats.gaussian_kde(yd, bw_method)
+        # get the pdf function for the y_data, compute and normalize the pdf values to desired width
+        pdf = stats.gaussian_kde(y_data, bw_method)
+        x_pdf = pdf(y_grid)
+        x_pdf = x_pdf/x_pdf.max()*vw/2
 
-        # append the y values with a negated and reversed version (to form teh right patch)
-        yv = np.append(yp, yp[::-1])
+        # Build arrays that describe the patch points by appending reversed arrays (and negated for x)
+        x_patch = np.append(x_pdf, -x_pdf[::-1])  # negated and reversed to get the left side of the violin
+        y_patch = np.append(y_grid, y_grid[::-1]) # reversed to start left violin where right violin ended
 
-        # compute the pdf (x values) and normalize to desired width
-        xp = pdf(yp)
-        xp = xp/xp.max()*vw/2
-
-        # append the x-values with a negated and reversed version.
-        xv = np.append(xp, -xp[::-1])
-
-        p.patch((xv+ix), yv, alpha=1, color='lightgrey', line_color='black', line_width=1)
+        # add the patch to the plot
+        p.patch((x_patch + ix), y_patch, alpha=1, color='lightgrey', line_color='grey', line_width=1)
 
         ix += 1
+
+    # ******************
+    # *** Raw Points ***
+    # ******************
 
     # jitter categorical data (TODO:  should only be done if showpoints is True)
     if jitter_points:
@@ -159,7 +173,7 @@ def make_violinplot(x=None, y=None, data=None, jw=.3, vw=.8,
     # plot non-outliers
     if show_points:
         p.scatter(dt[~dt.outlier]['xc'], dt[~dt.outlier][y],
-                  color='black', marker='o', size=4, alpha=0.5, line_color=None)
+                  color='black', marker='o', size=4, alpha=1, line_color=None)
 
     # plot outliers
     if show_outliers:
--- test.py
+++ test.py
@@ -1,47 +1,41 @@
 from generate_df import *
-from summarize_df import *
 from bivar import *
 from bokeh.plotting import output_file, show, gridplot
 import seaborn as sns
 
 
 # generate some test data
-df = generate_test_dataframe(2000)
-
-# set x and y variables
-x, y = 'cv1', 'yv3'
-
-df.dropna(inplace=True)
+df = generate_test_dataframe(500)
 
 # output to static HTML file
 output_file("test.html", title="Test Plots")
 
 # make a few instances plots
-p1 = make_stripplot(x=x, y=y, data=df, jitter_points=False, jw=0.3)
-p2 = make_stripplot(x=x, y=y, data=df, jitter_points=True, jw=0.3)
+p1 = make_strip_plot(x='cv1', y='yv3', data=df)
+p2 = make_strip_plot(x='cv1', y='yv3', data=df, jitter_points=True, jw=0.3)
 
-p3 = make_boxplot(x, y, df, .3, .7, True, True, True)
-p4 = make_boxplot(x, y, df, .3, .5, False, False, True)
+p3 = make_box_plot(x='cv1', y='yv3', data=df)
 
-p5 = make_violinplot(x=x, y=y, data=df, jw=.2, vw=.8, bw_method=.2, jitter_points=True, show_points=True,
-                     show_outliers=True)
+p4 = make_box_plot(x='cv1', y='yv3', data=df, jw=.2, bw=.5, jitter_points=True,
+                   show_points=True, show_outliers=True)
 
-p6 = make_violinplot(x=x, y=y, data=df, jw=.2, vw=.7, jitter_points=False, show_points=False, show_outliers=True)
+p5 = make_violin_plot(x='cv1', y='yv3', data=df)
+
+p6 = make_violin_plot(x='cv1', y='yv3', data=df, jw=.2, vw=.7, jitter_points=True,
+                      show_points=True, show_outliers=True)
 
 # get some seaborn data for comparison
 tips = sns.load_dataset("tips")
-x, y = 'day', 'total_bill'
 
-st, dt = get_summary_stats('day', 'total_bill', tips)
+p7 = make_box_plot(x='day', y='total_bill', data=tips)
 
-p7 = make_boxplot(x=x, y=y, data=dt, jw=.15, bw=.7, jitter_points=True, show_points=True, show_outliers=True)
+p8 = make_box_plot(x='day', y='total_bill', data=tips, jw=.15, bw=.7, jitter_points=True,
+                   show_points=True, show_outliers=True)
 
-p8 = make_boxplot(x=x, y=y, data=dt, jw=.15, bw=.7, jitter_points=False, show_points=False, show_outliers=True)
+p9 = make_violin_plot(x='day', y='total_bill', data=tips)
 
-p9 = make_violinplot(x=x, y=y, data=dt, jw=.2, vw=.8, jitter_points=False, show_points=True, show_outliers=True,
-                     bw_method=.3)
-
-p10 = make_violinplot(x=x, y=y, data=dt, jw=0, vw=.6, jitter_points=False, show_points=False, show_outliers=True)
+p10 = make_violin_plot(x='day', y='total_bill', data=tips, jw=.15, vw=.6, jitter_points=True,
+                       show_points=True, show_outliers=True)
 
 # render the plots
 p = gridplot([[p1, p2], [p3, p4], [p5, p6], [p7, p8], [p9, p10]])
